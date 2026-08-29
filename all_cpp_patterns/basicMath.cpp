896d9cdb91dd4a3169cf0ed53e1f25c02ff7dd6a
Nilesh Baraiya <nlbaraiya84@gmail.com>
2026-08-29 09:43:41 +0530

    add file

--- all_cpp_patterns/basicMath.cpp
+++ all_cpp_patterns/basicMath.cpp
@@ -1,14 +1,42 @@
 #include<iostream>
 using namespace std;
-int main(){
 
-    int n;
-    cin >> n;
-
+void digitcount(int n){
     while(n > 0){
         int lastdigit = n%10;
         cout << lastdigit;
         n/=10;
     }
-    return 0;
+}
+
+void reversenumber(int n){
+    int reverse=0;
+    while(n > 0){
+        int lastdigit = n%10;
+        reverse = (reverse *10)+lastdigit;
+        n/=10;
+    }
+    cout << reverse;
+}
+
+void palidrome(int n){
+    int dup = n;
+    int reverse = 0;
+    while(n>0){
+        int lastdig = n%10;
+        reverse = (reverse*10)+lastdig;
+        n/=10;
+    }
+    if (reverse == dup){
+        cout << "true";
+    }else{
+        cout << "false";
+    }
+}
+
+int main(){
+
+    int n;
+    cin >> n;
+    palidrome(n);
 }
