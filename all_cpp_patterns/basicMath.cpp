9447e6c8788c37c8dad49f6e79b4e40dc00f9bd7
Nilesh Baraiya <nlbaraiya84@gmail.com>
2026-08-30 10:11:12 +0530

    add file

--- all_cpp_patterns/basicMath.cpp
+++ all_cpp_patterns/basicMath.cpp
@@ -1,6 +1,5 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
-#include<cmath>
 
 void digitcount(int n){
     while(n > 0){
@@ -52,9 +51,42 @@ void armstromNum(int n){
     }
 }
 
+void printdevisior(int n){
+    vector<int> ls;
+    for (int i=1; i<=sqrt(n); i++){
+
+        if (n %i == 0){
+           ls.push_back(i); 
+           if (n/i != i){
+            ls.push_back(n/i);
+           }
+        }
+    }
+    sort(ls.begin(),ls.end());
+    for (auto it : ls) {
+        cout << it << " ";
+    }
+        
+}
+
+void gcd(int n1,int n2){
+   while(n1>0 && n2>0){
+    if ( n1 > n2){
+        n1 = n1 % n2;
+    } else {
+        n2 = n2 % n1;
+    }
+   }
+   if ( n1==0) cout << n2;
+   else n1;
+}
+
 int main(){
 
-    int n;
-    cin >> n;
-    armstromNum(n);
+    int n1;
+    cin >> n1;
+    int n2;
+    cin >> n2;
+    
+    gcd(n1,n2);
 }
